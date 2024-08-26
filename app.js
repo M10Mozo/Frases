45b76c123bdc0dca676dd45bf89832fc1446713e
M10Mozo <149447491+M10Mozo@users.noreply.github.com>
2024-08-25 19:24:46 -0600

    Tarea de frases

---
+++ app.js
@@ -0,0 +1,66 @@
+const fracesOriginales = [
+    { frace: '"los programas"', autor: 'Harold 1' },
+    { frace: '"los programas 1"', autor: 'Harold 2' },
+    { frace: '"los programas 2"', autor: 'Harold 3' },
+    { frace: '"los programas 3"', autor: 'Harold 4' },
+    { frace: '"los programas 4"', autor: 'Harold 5' },
+];
+
+const { createApp, ref } = Vue;
+
+const app = createApp({
+    setup() {
+        const fraces = ref(fracesOriginales);
+        const nuevafrace = ref('');
+        const nuevoautor = ref('');
+        const mostrarModal = ref(false);
+        const fraseEditada = ref({ frace: '', autor: '' });
+        let indiceEdicion = null;
+
+        const add = () => {
+            if (nuevafrace.value && nuevoautor.value) {
+                fraces.value.push({ frace: nuevafrace.value, autor: nuevoautor.value });
+                nuevafrace.value = '';
+                nuevoautor.value = '';
+            }
+        };
+
+        const eliminar = (numeros) => {
+            fraces.value.splice(numeros, 1);
+        };
+
+        const abrirModal = (numeros) => {
+            mostrarModal.value = true;
+            indiceEdicion = numeros;
+            // Aquí se vacían los campos del modal
+            fraseEditada.value = { frace: '', autor: '' };
+        };
+
+        const cerrarModal = () => {
+            mostrarModal.value = false;
+            indiceEdicion = null;
+        };
+
+        const actualizarFrase = () => {
+            if (fraseEditada.value.frace && fraseEditada.value.autor) {
+                fraces.value.splice(indiceEdicion, 1, { frace: fraseEditada.value.frace, autor: fraseEditada.value.autor });
+                cerrarModal();
+            }
+        };
+
+        return {
+            fraces,
+            nuevafrace,
+            nuevoautor,
+            mostrarModal,
+            fraseEditada,
+            add,
+            eliminar,
+            abrirModal,
+            cerrarModal,
+            actualizarFrase,
+        };
+    }
+});
+
+app.mount('#myapp');
